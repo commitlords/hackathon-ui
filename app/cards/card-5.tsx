769638d7c44054cd6f32e7058c870e766d7e83d3
Mayank Kumrawat <mkmayank1806@gmail.com>
2025-07-24 18:38:50 +0530

    minor fixes

--- app/cards/card-5.tsx
+++ app/cards/card-5.tsx
@@ -1,18 +1,20 @@
 "use client";
 import { Card } from "flowbite-react";
+import { useTranslation } from "react-i18next";
 
 export function Component() {
+  const { t } = useTranslation();
   return (
     <Card
       className="max-w-sm"
-      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgAlt={t('card5.imgAlt')}
       imgSrc="/card-img-5.png"
     >
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        Card 5: The Rise of Quantum Computing
+        {t('card5.title')}
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        {t('card5.desc')}
       </p>
     </Card>
   );
